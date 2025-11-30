4f5de99e400e7262fd11d22fdefbafbd940e8ce2
indra-bobo <indra@jxboard.co.id>
2025-12-01 01:20:14 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAcXnOIcROvGi590KXD0O8iE6Ynx1mArrc",
+  authDomain: "absensi-cemara.firebaseapp.com",
+  projectId: "absensi-cemara",
+  storageBucket: "absensi-cemara.firebasestorage.app",
+  messagingSenderId: "668372434916",
+  appId: "1:668372434916:web:fd4ea891098238313f11f1",
+};
